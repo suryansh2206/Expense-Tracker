6f3b47b79c063e9ee132a8d3e4be07fa0a948100
suryansh2206 <suryansh804.sg@gmail.com>
2023-05-15 14:32:33 +0530

    Verify Email

--- src/Components/Pages/Welcome.js
+++ src/Components/Pages/Welcome.js
@@ -1,17 +1,49 @@
 import React, { Fragment } from "react";
-import "./Welcome.css";
+import classes from "./Welcome.module.css";
 import { NavLink } from "react-router-dom";
 
 const Welcome = () => {
+  const verifyHandler = () => {
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyCY-VGJzQO4PuIAWLAzUqOd4c2XvpMOQFs",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          requestType: "VERIFY_EMAIL",
+          idToken: localStorage.getItem("token"),
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      if (res.ok) {
+        res.json().then((data) => console.log(data));
+      } else {
+        res.json().then((data) => {
+          alert(data.error.message);
+        });
+      }
+    });
+  };
+
   return (
     <Fragment>
-      <header className="header">
-        <div className="left-column">Welcome to Expense Tracker</div>
-        <div className="rigth-column">
-          <div>Your Profile is incomplete</div>
-          <NavLink activeClassName="active" className="link" to='/updateprofile'>
+      <header className={classes.header}>
+        <div className={classes.left}>Welcome to Expense Tracker</div>
+        <div className={classes.rigth}>
+          Your Profile is incomplete{"  "}
+          <NavLink
+            activeClassName="active"
+            className="link"
+            to="/updateprofile"
+          >
             Complete now
           </NavLink>
+          <div>
+            Verify your email now{"  "}
+            <button onClick={verifyHandler}>Verify</button>
+          </div>
         </div>
       </header>
     </Fragment>
